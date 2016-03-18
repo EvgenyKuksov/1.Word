40be9aa671330d5ad7e213b3ccebdaa3ecf8e784
EvgenyKuksov <Nakaut47@Gmail.com>
2016-03-18 19:04:01 +0700

    Added files via upload

--- Unit1.pas
+++ Unit1.pas
@@ -69,24 +69,28 @@ procedure TForm1.Button1Click(Sender: TObject);
 var
   masStrok: array of string;
 begin
-  SetLength(masStrok, 13);
-  masStrok[0] := Edit1.Text;
-  masStrok[1] := Edit2.Text;
-  masStrok[2] := Edit3.Text;
-  masStrok[3] := Edit4.Text;
-  masStrok[4] := Edit5.Text;
-  masStrok[5] := Edit6.Text;
-  masStrok[6] := Edit7.Text;
-  masStrok[7] := Edit8.Text;
-  masStrok[8] := Edit9.Text;
-  masStrok[9] := Edit10.Text;
-  masStrok[10] := DateToStr(DateTimePicker1.DateTime);
-  masStrok[11] := DateToStr(DateTimePicker2.DateTime);
-//  masStrok[12] := ifthen(RadioButton1.Checked, 'Мужской', 'Женский');
-  if (RadioButton1.Checked) then masStrok[12]:='Мужской'
-  else if (RadioButton2.Checked) then masStrok[12]:='Женский';
+  if ((Sender as TEdit).Text = '') then ShowMessage('Заполните все поля ввода данных')
+  else
+  begin
+    SetLength(masStrok, 13);
+    masStrok[0] := Edit1.Text;
+    masStrok[1] := Edit2.Text;
+    masStrok[2] := Edit3.Text;
+    masStrok[3] := Edit4.Text;
+    masStrok[4] := Edit5.Text;
+    masStrok[5] := Edit6.Text;
+    masStrok[6] := Edit7.Text;
+    masStrok[7] := Edit8.Text;
+    masStrok[8] := Edit9.Text;
+    masStrok[9] := Edit10.Text;
+    masStrok[10] := DateToStr(DateTimePicker1.DateTime);
+    masStrok[11] := DateToStr(DateTimePicker2.DateTime);
+  //  masStrok[12] := ifthen(RadioButton1.Checked, 'Мужской', 'Женский');
+    if (RadioButton1.Checked) then masStrok[12]:='Мужской'
+    else if (RadioButton2.Checked) then masStrok[12]:='Женский';
 
-  createDoc(masStrok);
+    createDoc(masStrok);
+  end;
 end;
 
 end.
